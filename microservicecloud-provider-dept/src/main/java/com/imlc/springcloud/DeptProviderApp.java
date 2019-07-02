08f8f41aba58f857148f055ac010d67e050d0578
ImChong <imliuchong@hotmail.com>
2019-07-02 18:19:56 +0800

    bug

--- microservicecloud-provider-dept/src/main/java/com/imlc/springcloud/DeptProviderApp.java
+++ microservicecloud-provider-dept/src/main/java/com/imlc/springcloud/DeptProviderApp.java
@@ -2,12 +2,16 @@ package com.imlc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
+ *  将服务启动后自动注册进Eureka服务
+ *
  * @Auther: L.C
  * @Date: 2019-07-02 10:13
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProviderApp {
 
     public static void main(String[] args) {
