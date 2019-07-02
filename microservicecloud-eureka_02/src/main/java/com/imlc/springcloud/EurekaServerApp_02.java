c44ab6905e40a0b8d6e05bbd5bb429db498e32d0
CrabMan <imliuchong@hotmail.com>
2019-07-03 07:11:11 +0800

    bug

--- microservicecloud-eureka_02/src/main/java/com/imlc/springcloud/EurekaServerApp_02.java
+++ microservicecloud-eureka_02/src/main/java/com/imlc/springcloud/EurekaServerApp_02.java
@@ -5,17 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Eureka服务端启动类，接受其他微服务注册
- *
  * @Auther: L.C
- * @Date: 2019-07-02 14:26
+ * @Date: 2019-07-02 22:54
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApp {
+public class EurekaServerApp_02 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApp.class, args);
+        SpringApplication.run(EurekaServerApp_02.class, args);
     }
 
 }
