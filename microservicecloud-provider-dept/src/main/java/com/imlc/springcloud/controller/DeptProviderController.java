63723c5b3af3cf229dc30b09e89e1268ec9bb419
ImChong <imliuchong@hotmail.com>
2019-07-02 18:38:54 +0800

    bug

--- microservicecloud-provider-dept/src/main/java/com/imlc/springcloud/controller/DeptProviderController.java
+++ microservicecloud-provider-dept/src/main/java/com/imlc/springcloud/controller/DeptProviderController.java
@@ -2,7 +2,11 @@ package com.imlc.springcloud.controller;
 
 import com.imlc.springcloud.entity.DeptEntity;
 import com.imlc.springcloud.service.DeptService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,9 +20,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/provider/dept")
 public class DeptProviderController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private DeptService deptService;
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public boolean addDept(@RequestBody DeptEntity deptEntity) {
@@ -33,6 +40,17 @@ public class DeptProviderController {
     @RequestMapping(value = "/list", method = RequestMethod.GET )
     public List<DeptEntity> getDeptList() {
         return deptService.getDeptList();
+    }
+
+    @RequestMapping("/discovery")
+    public Object dicorvery() {
+        List<String> list = client.getServices();
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD_DEPT");
+        for (ServiceInstance server : srvList) {
+            logger.info(server.getServiceId() + "\t" + server.getHost() + "\t" + server.getPort() + "\t" + server.getUri());
+        }
+        return this.client;
     }
 
 }
