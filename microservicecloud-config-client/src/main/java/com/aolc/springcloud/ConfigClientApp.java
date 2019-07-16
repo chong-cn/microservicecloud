203963a199d477de26db55ac16dc14234773fffd
ImChong <imliuchong@hotmail.com>
2019-07-16 17:16:29 +0800

    config refresh

--- microservicecloud-config-client/src/main/java/com/aolc/springcloud/ConfigClientApp.java
+++ microservicecloud-config-client/src/main/java/com/aolc/springcloud/ConfigClientApp.java
@@ -2,12 +2,14 @@ package com.aolc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * @Auther: L.C
  * @Date: 2019-07-12 14:28
  */
 @SpringBootApplication
+@RefreshScope
 public class ConfigClientApp {
 
     public static void main(String[] args) {
