75e58fed85f309397e634d92fcdb8db545b95594
ImChong <imliuchong@hotmail.com>
2019-07-17 08:53:47 +0800

    config eureka client

--- microservicecloud-config/src/main/java/com/aolc/springcloud/ConfigServerApp.java
+++ microservicecloud-config/src/main/java/com/aolc/springcloud/ConfigServerApp.java
@@ -2,16 +2,12 @@ package com.aolc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Auther: L.C
  * @Date: 2019-07-11 12:40
  */
 @SpringBootApplication
-@EnableConfigServer
-@EnableDiscoveryClient
 public class ConfigServerApp {
 
     public static void main(String[] args) {
