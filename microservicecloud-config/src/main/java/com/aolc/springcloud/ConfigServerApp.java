8494d63d8ee36b779df2f355b7ae31a65ce8bb1a
aolc <imliuchong@hotmail.com>
2019-07-22 11:22:28 +0800

    bug

--- microservicecloud-config/src/main/java/com/aolc/springcloud/ConfigServerApp.java
+++ microservicecloud-config/src/main/java/com/aolc/springcloud/ConfigServerApp.java
@@ -2,14 +2,14 @@ package com.aolc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Auther: L.C
  * @Date: 2019-07-11 12:40
  */
 @SpringBootApplication
-//@EnableConfigServer
+@EnableConfigServer
 public class ConfigServerApp {
 
     public static void main(String[] args) {
